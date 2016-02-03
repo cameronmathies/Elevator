a80a18c3d5ba6a6a62b95c2368fbd6147730d0df
cameronmathies <cmathies@IT005.mr.preble.com>
2016-02-03 13:08:22 -0500

    Main Menu
    The buttons are all programmed. Just awaitingthe textures to put on
    them.

---
+++ Main Game Project/Assets/Buttons.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Buttons : MonoBehaviour {
+
+	public void Play()
+    {
+        Application.LoadLevel("Lobby");
+    }
+    public void Credits()
+    {
+        Application.LoadLevel("Credits");
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
