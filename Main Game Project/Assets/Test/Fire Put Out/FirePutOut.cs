e718f841a0c027e248d3c1bcad19d1a29a5b4015
cameronmathies <cmathies@IT005.mr.preble.com>
2016-02-08 13:02:57 -0500

    Fire
    Put out fire with fire extinguisher via tags.

---
+++ Main Game Project/Assets/Test/Fire Put Out/FirePutOut.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FirePutOut : MonoBehaviour {
+    
+    
+
+    // Use this for initialization
+    void FixedUpdate() {
+        if (Input.GetKey(KeyCode.G))
+        {
+            yourObject.tag = "Foam";
+
+        }
+        if (Input.GetKey(KeyCode.H))
+        {
+            yourObject.tag = "Foamless";
+
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Main Game Project/Assets/Test/Fire Put Out/PlayerMove.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : MonoBehaviour
+{
+
+    public float moveSpeed;
+    public float speed = 10f;
+	public float RotateSpeed;
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+        }
+        
+	}
+}
