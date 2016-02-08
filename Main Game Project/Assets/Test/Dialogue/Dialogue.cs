0b2e7a438932a0f55098c78cb7e467ff454bd5a7
cameronmathies <cmathies@IT005.mr.preble.com>
2016-02-08 12:18:19 -0500

    Dialogue Demo
    I've made a Dialogue Demo to take a look at how to go about dialogue.

---
+++ Main Game Project/Assets/Test/Dialogue/Dialogue.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dialogue : MonoBehaviour {
+    public AudioSource[] sounds;
+    public AudioSource noise1;
+    public AudioSource noise2;
+    // Use this for initialization
+    void Start () {
+        sounds = GetComponents<AudioSource>();
+        noise1 = sounds[0];
+        noise2 = sounds[1];
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.Y))
+        {
+            noise1.Play();
+        }
+        if (Input.GetKey(KeyCode.N))
+        {
+            noise2.Play();
+        }
+
+    }
+}
