12125d705d91ca92f81089164880009443c78368
cameronmathies <cmathies@IT005.mr.preble.com>
2016-02-11 12:07:10 -0500

    Pick Up
    You can now pickup a fire extinguisher.

---
+++ Main Game Project/Assets/Test/Fire Put Out/FireExtinguisherPickUp.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireExtinguisherPickUp : MonoBehaviour {
+
+    public GameObject Item;
+
+    void OnTriggerEnter(Collider other)
+    {
+        Item.SetActive(false);
+
+    }
+}
