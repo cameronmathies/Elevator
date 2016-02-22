917f9a772f3610864bac49d0a515c63343bbf2a1
cameronmathies <cmathies@IT005.mr.preble.com>
2016-02-22 11:50:08 -0500

    HeathBar
    Heath is shown in % and on bar.

---
+++ Main Game Project/Assets/Test/Preble/Helth.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class Helth : MonoBehaviour {
+    public Slider HealthBar;
+    public int Health;
+    public Text HealthT;
+
+    // Use this for initialization
+    void Start () {
+        Health = 100;
+	}
+	void OnTriggerStay(Collider col)
+    {
+        if(col.gameObject.tag == "Bad")
+        {
+            Health -= 2;
+        }
+    }
+	// Update is called once per frame
+	void Update () {
+	if(HealthBar.name == "HeathBar")
+        {
+            HealthBar.value = Health;
+        }
+        HealthT.text = Health + "%";
+
+        if(Health < 0)
+        {
+            Application.LoadLevel("Main Menuss");
+        }
+    }
+}
