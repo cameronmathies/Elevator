e780fd4e229301e57cc9a4673ffa8040ce309a94
cameronmathies <cmathies@IT005.mr.preble.com>
2016-03-01 13:25:36 -0500

    Why No Pic

--- Main Game Project/Assets/Test/Fire Put Out/Bullet.cs
+++ Main Game Project/Assets/Test/Fire Put Out/Bullet.cs
@@ -44,7 +44,7 @@ public class Bullet : MonoBehaviour
     void FireBall()
     {
         Rigidbody clone;
-        clone = Instantiate(projectilePrefab, spawnPoint.transform.position, spawnPoint.transform.rotation) as Rigidbody;
+        clone = Instantiate(projectilePrefab, transform.position, transform.rotation) as Rigidbody;
     }
     //IEnumerator fire()
     //{
@@ -61,7 +61,7 @@ public class Bullet : MonoBehaviour
         if (timer <= 0)
         {
             Rigidbody clone;
-            clone = Instantiate(projectilePrefab, spawnPoint.transform.position, spawnPoint.transform.rotation) as Rigidbody;
+            clone = Instantiate(projectilePrefab, transform.position, transform.rotation) as Rigidbody;
         
         //  clone = Instantiate(projectilePrefab, transform.position, transform.rotation) as Rigidbody;
 
