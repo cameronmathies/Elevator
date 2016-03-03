baaac644b3c4c4e5087a562e47381fa9e2588ab1
cameronmathies <cmathies@IT005.mr.preble.com>
2016-03-03 13:34:34 -0500

    update

---
+++ Main Game Project/Assets/Test/Preble/PlayerShot.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerShot : MonoBehaviour {
+    public GameObject spawnPoint;
+    public Rigidbody2D projectile1;
+    public AudioSource shot;
+    public GameObject projectilePrefab;
+    public GameObject Parent;
+    public float ShotSpeed;
+    public float timer;
+    public float nextFire = -11.0f;
+    public float timerCode = 5f;
+    public GameObject m_Enemy;
+    public float m_SpawnTime = 3f;
+    public Transform[] m_SpawnPoints;
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            nextFire = nextFire + 1;
+            Rigidbody hitPlayer;
+            hitPlayer = Instantiate(projectile1, transform.position, transform.rotation) as Rigidbody;
+            hitPlayer.velocity = transform.TransformDirection(Vector2.left * ShotSpeed);
+            // AudioSource audio = GetComponent<AudioSource>();
+            //  audio.Play();
+        }
+    }
+}
