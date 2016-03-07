fe02d23e2327c05381998fdb91a1b0eabdb5714b
cameronmathies <cmathies@mr.preble.com>
2016-03-07 12:03:07 -0500

    BulletDel

---
+++ Main Game Project/Assets/Test/Preble/BulletDelete.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletDelete : MonoBehaviour
+{
+    public GameObject PlayerFireBall;
+    public GameObject PrebleFireBall;
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Bad")
+        {
+
+            Destroy(PlayerFireBall);
+        }
+
+        if (other.gameObject.tag == "Good")
+        {
+
+            Destroy(PrebleFireBall);
+
+        }
+    }
+}
